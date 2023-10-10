8f737ce47acebb07e5fd5ac50e51e937598ae212
Raphael Lima <raphaeldesousalm@gmal.com>
2023-10-10 15:12:09 -0300

    feat: add animation sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -13,10 +13,10 @@ const Sidebar = ({open, onClose, children}: Props) => {
     return(
         <section 
         className={`fixed top-0 left-0 bottom-0 text-white 
-        ${open ? 'w-screen bg-gray-600/75 md:w-64 md:static' : 'w-0'}`}>
+        ${open ? 'w-screen bg-gray-600/75' : 'w-0'} md:w-64 md:static`}>
 
-        <div className={`flex h-screen ${open ? 'ml-0' : '-ml-96'}`}>
-            <div>Barra mesmo</div>
+        <div className={`transition-all duration-200 flex h-screen ${open ? 'ml-0' : '-ml-96'}`}>
+            <div className="flex flex-col w-64 p-2 bg-gray-900">Barra mesmo</div>
             <div 
                 className="flex justify-center items-center w-10 h-10 cursor-pointer md:hidden"
                 onClick={onClose}
