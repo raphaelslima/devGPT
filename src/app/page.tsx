fa0035fc5094e0bd9b369a54ee8db8bddb5c7216
Raphael Lima <raphaeldesousalm@gmal.com>
2023-10-11 15:28:20 -0300

    feat: add create chat and send msg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import {v4 as uuidv4 } from 'uuid'
 
 //Components
 import Header from "@/components/Header";
@@ -13,39 +14,64 @@ import ChatFooter from "@/components/ChatFooter";
 
 export default function Home() {
 
-  const [sidebarOpened, setSidebarOpened] = useState(false)
-  const [AILoading, setAILoading] = useState(false)
-  const [chatActive, setChatActive] = useState<Chat>({
-    id: '123',
-    title: 'bla bla',
-    messages: [
-      {
-      id: '99',
-      author: 'me',
-      body: 'bla bla'
-      },
-      {
-        id: '11',
-        author: 'ai',
-        body: 'blu blu'
-        }
-  ]
-  })
+  const [sidebarOpened, setSidebarOpened] = useState(false);
+  const [chatList, setChatList] = useState<Chat[]>([]);
+  const [chatActiveId, setChatActiveId] = useState('');
+  const [AILoading, setAILoading] = useState(false);
+  const [chatActive, setChatActive] = useState<Chat>();
 
-  const closeSidebar = () => setSidebarOpened(false)
-  const openSidebar = () => setSidebarOpened(true)
-  
+  useEffect(()=> {
+    setChatActive(chatList.find((item) => item.id === chatActiveId))
+  }, [chatActiveId, chatList])
+
+  const closeSidebar = () => setSidebarOpened(false);
+  const openSidebar = () => setSidebarOpened(true);
+
   const handleClearConversation = () => {
-
-  }
+    if(AILoading) return;
+    setChatActiveId('');
+    setChatList([]);
+  };
 
   const handleNewChat = () => {
+    if(AILoading) return;
+    setChatActiveId('');
+    closeSidebar()
+  };
 
-  }
+  const handleSendMessage = (msg: string) =>{
+    if(!chatActiveId){
 
-  const handleSendMessage = () =>{
+      let newChatId = uuidv4();
 
-  }
+      setChatList([ {
+        id: newChatId,
+        title: msg,
+        messages: [
+          {
+            id: uuidv4(),
+            author: 'me',
+            body: msg
+          }
+        ]
+      } ,...chatList]);
+
+      setChatActiveId(newChatId);
+    } else{
+
+      let chatListClone = [...chatList];
+      let chatIndex = chatListClone.findIndex((item)=> item.id === chatActiveId)
+      chatListClone[chatIndex].messages.push({
+        id: uuidv4(),
+        author: 'me',
+        body: msg
+      })
+
+      setChatList(chatListClone)
+    }
+
+    setAILoading(true)
+  };
 
   return (
     <main className="flex min-h-screen bg-gpt-gray">
